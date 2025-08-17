7254e89a3bb42e4320615555d734e72105e5356b
7844Pritam <pritamsharma7844@gmail.com>
2025-08-17 07:41:21 +0530

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/LandingPage.jsx';
-import OurDirectorPage from './pages/OurDirectorPage.jsx';
-import Layout from './componants/Layout.jsx';
-import CompanyProfilePage from './pages/CompanyProfilePage.jsx';
-import CareersPage from './pages/CareerPage.jsx';
-import AdminRoutes from './routes/AdminRoutes.jsx';
-import ProtectedAdmin from './admin/components/ProtectedAdmin.jsx';
-import ServiceDetailPage from './pages/ServiceDetailPage.jsx';
-import ContactPage from './pages/ContactUsPage.jsx';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage.jsx";
+import OurDirectorPage from "./pages/OurDirectorPage.jsx";
+import Layout from "./componants/Layout.jsx";
+import CompanyProfilePage from "./pages/CompanyProfilePage.jsx";
+import CareersPage from "./pages/CareerPage.jsx";
+import AdminRoutes from "./routes/AdminRoutes.jsx";
+import ProtectedAdmin from "./admin/components/ProtectedAdmin.jsx";
+import ServiceDetailPage from "./pages/ServiceDetailPage.jsx";
+import ContactPage from "./pages/ContactUsPage.jsx";
 
 function App() {
   return (
@@ -18,9 +18,8 @@ function App() {
         <Route path="directors" element={<OurDirectorPage />} />
         <Route path="profile" element={<CompanyProfilePage />} />
         <Route path="career" element={<CareersPage />} />
-<Route path="/service/:title" element={<ServiceDetailPage />} />
-                <Route path="/contact-us" element={<ContactPage />} />
-
+        <Route path="/service/:title" element={<ServiceDetailPage />} />
+        <Route path="/contact-us" element={<ContactPage />} />
       </Route>
 
       <Route
